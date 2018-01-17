bbb9cc9518a32d352e63f20422a47139b5a712e8
mitsumisi123 <mitsumisi123@gmail.com>
2018-01-17 18:21:40 +0700

    Update and rename chat_server.c to server.c

--- tcp/server.c
+++ tcp/server.c
@@ -4,8 +4,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-
+#include <unistd.h>
 int main() {
+	printf("[+]Server Socket Created Sucessfully.\n");
     int ss, cli, pid;
     struct sockaddr_in ad;
     char s[100];
@@ -20,36 +21,47 @@ int main() {
     ad.sin_addr.s_addr = INADDR_ANY;
     ad.sin_port = htons(12345);
     bind(ss, (struct sockaddr *)&ad, ad_length);
+    printf("bind successfully\n");
 
     // then listen
-    listen(ss, 0);
+    listen(ss, 2);
+    printf("listening...\n");
 
     while (1) {
         // an incoming connection
         cli = accept(ss, (struct sockaddr *)&ad, &ad_length);
 
         pid = fork();
-        if (pid == 0) {
+        if (pid == 0) 
+	{
             // I'm the son, I'll serve this client
-            printf("client connected\n");
-            while (1) {
-                // it's client turn to chat, I wait and read message from client
-                read(cli, s, sizeof(s));
-                printf("client says: %s\n",s);
-
-                // now it's my (server) turn
-                printf("server>", s);
-                scanf("%s", s);
-                write(cli, s, strlen(s) + 1);
-            }
-            return 0;
+            	FILE *fp;
+         	int ch = 0;
+		char buffer[512];
+           	fp = fopen("labwork1_recv.txt","a");            
+            	int words;
+		read(cli, &words, sizeof(int));
+            	//printf("Passed integer is : %d\n" , words);      //Ignore , Line for Testing
+          	while(ch != words)
+       	   	{
+        	 	read(cli ,buffer, 512); 
+	   	 	fprintf(fp , " %s" , buffer);   
+		 	//printf(" %s %d "  , buffer , ch); //Line for Testing , Ignore
+		 	ch++;
+	   	}
+     		printf("The file was received successfully\n");
+	   	
+	
+            	return 0;
         }
         else {
             // I'm the father, continue the loop to accept more clients
+		printf("HI");
             continue;
         }
+	// disconnect
+    	close(cli);
     }
-    // disconnect
-    close(cli);
+    
 
 }
